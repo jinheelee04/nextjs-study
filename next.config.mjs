020f4522ec54dfe518abb02fdbb017c9e01e830e
jinheelee04 <2pro.jlee@gmail.com>
2025-01-25 20:34:24 +0900

    2.1 강의 노트  추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+/**
+ * reactStrictMode
+ * 리액트앱에 존재하는 잠재적인 문제를 검사하기 위해서
+ * 개발모드로 실행했을 때 컴포넌트를 2번 실행하게 된다.
+ */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, 
 };
 
 export default nextConfig;
