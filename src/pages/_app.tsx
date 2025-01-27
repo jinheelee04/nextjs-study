a98d21137302cc9ee3130c63e2ef4132a8b67181
jinheelee04 <2pro.jlee@gmail.com>
2025-01-27 14:07:48 +0900

    2.3 네비게이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 /**
  * 리액트의 App 컴포넌트와 동일한 역활
  * 루트컴포넌트 (모든페이지들의 부모 컴포넌트)
@@ -10,10 +11,21 @@ import type { AppProps } from "next/app"
  * @returns
  */
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test");
+  };
   return (
     <>
-      <header>global header</header>
+      <header>
+        <Link href={"/"}>index</Link>&nbsp;
+        <Link href={"/search"}>search</Link>&nbsp;
+        <Link href={"/book/1"}>book/1</Link>&nbsp;
+        <div>
+          <button onClick={onClickButton}>/test 페이지 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
