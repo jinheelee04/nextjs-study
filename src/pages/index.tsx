2d4f6a046473187a9d5ba6b46ddce1b5344d5170
jinheelee04 <2pro.jlee@gmail.com>
2025-01-27 18:31:31 +0900

    2.9 UI 구현하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
-
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 /**
  * module css 를 사용하면 자동으로 유니크한 클래스네임을 만들어준다.
  * 각 페이지에 적용시켜야할 css인 경우 module을 사용해야한다.
@@ -10,10 +11,20 @@ import { ReactNode } from "react";
  */
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
