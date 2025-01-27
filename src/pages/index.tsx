a8dc6422fc8c9517f0a9283fe888c5000433851f
jinheelee04 <2pro.jlee@gmail.com>
2025-01-27 16:32:23 +0900

    2.6 스타일링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import style from "./index.module.css";
+
+/**
+ * module css 를 사용하면 자동으로 유니크한 클래스네임을 만들어준다.
+ * 각 페이지에 적용시켜야할 css인 경우 module을 사용해야한다.
+ * 충돌이 발생할 수 있기 때문이다.
+ * @returns
+ */
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
     </>
-  )
+  );
 }
