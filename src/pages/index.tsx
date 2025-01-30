c7834b5041f8530f6fc1b1dd937724c46922ea80
jinheelee04 <2pro.jlee@gmail.com>
2025-01-30 15:18:04 +0900

    2.12 SSR실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,27 @@ import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
 // 넥스트에서 약속된 함수를 export하면
 // ssr 방식으로 렌더링 되도록 설정된다.
 // 서버 환경에서만 실행됨, console.log, window 객체 실행 안됨
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-  const data = "hello";
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+export const getServerSideProps = async () => {
+  // await 으로 조회하면 순차적으로(직렬적으로) 조회함
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
+
+  // Promise.all을 사용하면 병렬로 동시에 데이터를 조회해 올 수 있다.
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
@@ -26,29 +37,27 @@ export const getServerSideProps = () => {
  * @returns
  */
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // 서버에서 한번, 브라우저에서 한번 총 2번 실행됨
   // window 객체를 그대로 사용하면 오류남
   // window.location;
-  console.log(data);
-
   // useEffect는 컴포넌트가 마운트된 다음에 실행되기 때문에
   // 브라우저에서만 실행됨으로 window 객체 사용해도 오류가 안남
-  useEffect(() => {
-    console.log(window);
-  }, []);
+
+  console.log(allBooks);
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,27 @@
+import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
+import fetchBooks from "@/lib/fetch-books";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import BookItem from "@/components/book-item";
-export default function Page() {
+
+/**
+ * context에는 현재 브라우저로 부터 받은 요청에 대한 모든 정보가 포함되어 있다.
+ * @param context
+ * @returns
+ */
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return {
+    props: { books },
+  };
+};
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { q } = router.query;
   return (
